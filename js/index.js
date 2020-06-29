d5a73b1d5d62703e6eae41d4eaa7c8a2dee2b8cd
Aveline Wingfield <avelinewingfield@gmail.com>
2020-06-29 19:53:04 -0400

    Completed, but did it the cheap/easy way. I'll update it later.

--- js/index.js
+++ js/index.js
@@ -40,3 +40,58 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let title = document.querySelector('h1');
+title.innerHTML = `DOM <br> Is <br> Awesome`; //Is there an alternate? 
+
+let button = document.querySelector('button');
+button.textContent = "Get Started"
+
+let banner = document.getElementById('cta-img');
+banner.src = "img/header-img.png";
+
+let main = document.querySelector('.text-content');
+main.innerHTML = `<h4>Features</h4>
+<p>Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+    in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+    scelerisque quis.</p>`;
+
+let sub = document.querySelector("body > div > section.main-content > div.top-content > div:nth-child(2)");
+sub.innerHTML = `<h4>About</h4>
+<p>About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in
+    interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+    scelerisque quis.</p>`;
+
+let middleimg = document.getElementById('middle-img');
+middleimg.src = "img/mid-page-accent.jpg";
+
+let bottomtxt = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(1)");
+bottomtxt.innerHTML = `<h4>Services</h4>
+                    <p>Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,
+                        in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+                        scelerisque quis.</p>`;
+
+let midtxt = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(2)");
+midtxt.innerHTML = `<h4>Product</h4>
+<p>Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in
+    interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+    scelerisque quis.</p>`;
+
+let bottext = document.querySelector("body > div > section.main-content > div.bottom-content > div:nth-child(3)");
+bottext.innerHTML = `<h4>Vision</h4>
+<p>Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in
+    interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus
+    scelerisque quis.</p>`
+
+let contact = document.querySelector('.contact');
+contact.innerHTML = `<h4>Contact</h4>
+  <p>123 Way 456 Street<br>
+  Somewhere, USA</p>
+  <p>1 (888) 888-8888</p>
+  <p>sales@greatidea.io</p>`;
+
+  let copyright = document.querySelector("body > div > footer > p")
+  copyright.textContent = "Copyright Great Idea! 2018"
+
+
+
